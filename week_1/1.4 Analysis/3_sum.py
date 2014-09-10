88e0c1de65f1ec820763d469adc0ce447fdd501d
the-hobbes <phelan.vendeville@gmail.com>
2014-09-09 21:25:51 -0700

    Added notes to 3_sum. Also, check out gdocs notes for empirical running time analysis

--- week_1/1.4 Analysis/3_sum.py
+++ week_1/1.4 Analysis/3_sum.py
@@ -9,7 +9,12 @@
       30 + -20 + 10 = 0
       -40 + 40      = 0
       -10 + 10      = 0
+
+    Running Time is N^3
 '''
+from datetime import datetime
+
+
 class ThreeSum():
 
   def __init__(self, inpt):
@@ -19,24 +24,33 @@ class ThreeSum():
     '''
         Loop through self.inpt and determine the number of triples sum to 0.
         Return that number. 
+        Brute force, for small numbers. 
     '''
     inpt = self.inpt
     counter = 0
     N = len(inpt)
     
+    # triple for loop, gets each triple once and checks the sum.
     for i in range(N):
       for j in range(i + 1, N):
         for k in range(j + 1, N):
           if inpt[i] + inpt[j] + inpt[k] == 0:
             counter += 1
 
-    print counter
     return counter
 
 
 def main():
   inpt = [30, -40, -20, -10, 40, 0, 10, 5]
-  output = ThreeSum(inpt)
+  
+  # time it the operation
+  time_start = datetime.now()
+  output = ThreeSum(inpt) # the actual code we want to test
+  time_end = datetime.now()
+  time_elapsed = time_end - time_start
+  # milliseconds_elapsed = int(time_elapsed.total_seconds() * 1000)
+  print time_elapsed
+
   assert output.count() == 4
 
 if __name__ == '__main__':

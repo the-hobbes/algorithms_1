0e35a29c0c3fda1e22b7dd00c5fd534cd2765d54
Phelan Vendeville <phelan.vendeville@gmail.com>
2014-12-29 19:30:20 -0500

    added an implementation of a stack using arrays as the underlying data structures.

--- week_3/stack_array.py
+++ week_3/stack_array.py
@@ -4,36 +4,45 @@
   Create a stack, with an array as the underlying data structure.
 
   Complexity analysis:
-  - 
+  - a defect is that the stack overflows when N exceeds capacity. 
 '''
 
 class StackOfStrings():
   # stack implementation using an array
 
   
-  def __init__(self):
-    pass
+  def __init__(self, capacity):
+    # note that capacity is a cheat, to be fixed later
+    self.stack = [None] * capacity
+    self.top = 0
 
   def push(self, item):
-    pass
+    # add new item at s[n]
+    self.stack[self.top] = item
+    self.top = self.top + 1
 
   def pop(self):
-    pass
+    # remove item at s[n-1]
+    item = self.stack[self.top - 1]
+    self.stack[self.top] = None
+    self.top = self.top - 1
+    return item
 
   def is_empty(self):
-    pass
+    return self.top == 0
 
 
 def main():
   # client code/tests
-  stk = StackOfStrings()
+  stk = StackOfStrings(10)
   stk.push('a')
   stk.push('b')
   stk.push('c')
-  stk.pop
-  stk.pop
+  stk.pop()
+  stk.pop()
   assert stk.pop() == 'a'
   assert stk.is_empty() == True
 
 
 main()
+

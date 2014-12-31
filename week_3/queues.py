d4082840dddbbeacec298102ab67b6df00a11cc3
Phelan Vendeville <phelan.vendeville@gmail.com>
2014-12-31 12:28:31 -0500

    Added a shell for an implementation of queues using resizing arrays

--- week_3/queues.py
+++ week_3/queues.py
@@ -5,12 +5,17 @@
     enqueue(item)     insert a new string into queue
     dequeue()         remove/return least recently added string
     is_empty()        is the queue empty?
+
+  Head and tail:
+    Enqueue = add a new item at tail.
+    Dequeue = remove an item at head.
 '''
 
 class QueueOfStringsLinkedList():
   '''
     Queue =   
       first - node - node - node - last
+    Create a queue using a linked list as the data structure.
   '''
 
   class Node():
@@ -68,7 +73,43 @@ class QueueOfStringsLinkedList():
       node = node.nxt
 
 
+class QueueOfStringsResizingArray():
+  '''
+    Create a queue using a resizing array as the data structure.
+    I am using the same concept as in resizing_arrays.py.
+  '''
+
+
+  def __init__(self):
+    self.q = []
+    self.head = None
+    self.tail = None
+    self.capacity = None
+
+  def enqueue(self, item):
+    # add a new item at q[tail]
+    pass
+
+  def dequeue(self):
+    # remove an item from q[head]
+    pass
+
+  def resize(self, capacity):
+    new_array = [None] * capacity
+    for index in range(len(self.q)):
+      if self.q[index]:
+        new_array[index] = self.q[index]
+
+    return new_array
+
+  def is_empty(self):
+    pass
+
+  def print_queue(self):
+    pass
+
 def main():
+  print '---Linked List Queue:---'
   ll_queue = QueueOfStringsLinkedList() 
   ll_queue.enqueue('a')
   ll_queue.enqueue('b')
@@ -76,6 +117,11 @@ def main():
   ll_queue.print_queue()
   print '---Dequeue---'
   print ll_queue.dequeue()
+
+  print '---Array Queue---'
+  array_queue = QueueOfStringsResizingArray()
+  array_queue.enqueue('a')
+  array_queue.print_queue()
 
 
 main()

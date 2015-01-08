1ea33a591470e73811927e7cbffe8b3afe7da5f7
Phelan Vendeville <pheven@google.com>
2015-01-08 17:09:24 +0100

    working on explanation of shellsort
    Working on explanation of shell sort implementation, and small tweaks to
    comments on insertion sort.

--- week_4/insertion_sort.py
+++ week_4/insertion_sort.py
@@ -9,10 +9,10 @@ class InsertionSort():
         On each iteration, move a[i] into the appropriate position on its left.
         Everything to the left of i is sorted, and everything to the right we 
         have not seen yet. 
-        When i is less than wthe element to its left, exchange it with that 
+        When i is less than the element to its left, exchange it with that 
         element. Continue until the elements to the left of the pointer are in
         sorted order. This means that everything to the left of i is in order.
-        Repeate this until there are no more cards left to sort.
+        Repeate this until there are no more elements left to sort.
     '''  
 
     def __init__(self):
--- week_4/shell_sort.py
+++ week_4/shell_sort.py
@@ -2,31 +2,46 @@
 
 class ShellSort():
   '''
-  ''' 
+  '''
   def __init__(self, a):
     # argument: comparable a
-    self.N = len(a)
-    self.h = self.calculate_starting_h()
+    self.a = a
+    self.gap = self.calculate_start()
 
   def sort(self):
-    while self.h >= 1:
-      # h-sort the array
-      for i in range(self.h, self.N):
-        
-      self.h = self.h / 3 # move to the next increment
+    gap = self.gap
+    a = self.a
+    print 'original: ' + str(a)
+    # loop over gaps
+    while gap > 0:
+      print 'gap:' + str(self.gap) + '\n'
+      # insertion sort time, using the gap instead of -1
+      for i in range(gap, len(a)): # for the array given, gap starts at 4
+        val = a[i]
+        j = i
+        while j >= gap and a[j - gap] > val:
+          print 'j:' + str(j)
+          print 'a[j]: ' + str(a[j])
+          a[j] = a[j - gap]
+          j -= gap
+          print 'currently: ' + str(a)
+        a[j] = val
+      gap //= 3
 
-  def calculate_starting_h(self):
+    return a
+
+  def calculate_start(self):
     # calculate starting h using knuth's 3x + 1 method
     h = 1
-    while h < int(self.N / 3):
+    while h < len(self.a) // 3:
       h = (3 * h) + 1
-    
+
     return h
 
 
 def main():
-  sort_me = ['s', 'h', 'e', 'l', 'l', 's', 'o', 'r', 't']
+  sort_me = [1, 3, 78, 3, 6, 8, 5, 9, 0]
   shell_sort = ShellSort(sort_me)
-  shell_sort.sort()
+  print shell_sort.sort()
 
 main()

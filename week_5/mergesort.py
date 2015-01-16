aea2d644c8cb7416e981ea2719aad3a7d3a8cf9e
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-01-16 21:31:58 +0100

    implemented mergesort, added comments

--- week_5/mergesort.py
+++ week_5/mergesort.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 '''
   Mergesort implemented in python.
-  See notes for an explanation of the algorithm.
+  Continually splits a list in half. 
+  - If the list is empty or has one item, it is sorted by definition (the base
+  case).
+  - If the list has more than one item, we split the list and recursively
+  invoke a merge sort on both halves.
+  - Once the two halves are sorted, the fundamental operation (called a merge)
+  is performed. 
+  - Merging is the proces of taking two smaller sorted lists and combining them
+  to form a single, sorted, new list. 
 '''
 import math
 
@@ -12,52 +20,52 @@ class Mergesort():
     '''
       Recursively sort an array.
     '''
+    print('Splitting ', comparable)
+
+    # base case
     if len(comparable) < 2:
       return comparable
-    result = [0] * len(comparable)
+    
     mid = len(comparable) // 2
-    low_array = comparable[:mid]
-    high_array = comparable[mid:]
+    left_half = comparable[:mid]
+    right_half = comparable[mid:]
 
-    self.sort(low_array)
-    self.sort(high_array)
+    self.sort(left_half)
+    self.sort(right_half)
+  
+    # at this point, we assume the sublists are sorted, and can be merged
+    i, j, k = 0, 0, 0
+    
+    while i < len(left_half) and j < len(right_half):
+      # while we have items in both arrays, compare the two and pick the smaller
+      if left_half[i] < right_half[j]: # the item in the left list is smaller
+        comparable[k] = left_half[i]
+        i += 1 # increment i, as we've used it to put an item into the result
+      else: # the item on the right is smaller, or they are the same
+        comparable[k] = right_half[j]
+        j += 1 # increment j, as we've used it to put an item into the result
+      k = k + 1 # increment k, as we must fill the next slot it the result
 
-    http://interactivepython.org/runestone/static/pythonds/SortSearch/TheMergeSort.html
-
-    # actually do the sort
-    i = 0
-    j = 0
-    for k in range(len(comparable)):
-      print 'i is: ' + str(i)
-      print 'j is: ' + str(j)
-      print 'length of low_array is: ' + str(len(low_array))
-      print 'the low_array: '
-      print low_array
-      print 'length of high_array is: ' + str(len(high_array))
-      print 'the high_array: '
-      print high_array
-      print '\n'
-
-      if i > len(low_array):
-        result[k] = high_array[j]
-        j += 1
-      elif j > len(high_array):
-        result[k] = low_array[i]
-        i += 1
-      elif low_array[i] < high_array[j]:
-        result[k] = low_array[i]
-        i += 1
-      else:
-        result[k] = high_array[j]
-        j += 1
-
-    return result
-
+    while i < len(left_half):
+      # if there are any items left in the left half, put them into the result
+      comparable[k] = left_half[i]
+      i += 1
+      k += 1
+  
+    while j < len(right_half):
+      # if there are any items left in the right half, put them into the result
+      comparable[k] = right_half[j]
+      j += 1
+      k += 1
+    
+    print('Merging ', comparable)
+ 
+    return comparable
 
 def main():
   comparable = [2, 4, 5, 1, 0, 435, 5, 7, 9]
   ms = Mergesort()
-  print ms.sort(comparable)
+  print('The sorted list is: ', ms.sort(comparable))
 
 
 main()
